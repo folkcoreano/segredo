e6263aeebaaab029fc6373ed5fe845d5c726a3d4
yonas <folkcoreano@outlook.com>
2022-06-20 23:28:35 -0300

    eita

--- assets/books.36173f09.js
+++
@@ -1,1 +0,0 @@
-import{b as e,o,c as s}from"./index.c08f0506.js";const t={class:"category"},l={__name:"books",setup(c){return e().newPageTitle("Books"),(r,n)=>(o(),s("div",t,"Books!"))}};export{l as default};
